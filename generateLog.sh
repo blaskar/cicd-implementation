713bc629670a5b9ea2ca3a65c201dfd366fe7725
blaskar <30313971+blaskar@users.noreply.github.com>
2020-06-25 00:50:40 +0530

    Update generateLog.sh

--- generateLog.sh
+++ generateLog.sh
@@ -1,2 +1,3 @@
 ls -ltr > log.log
-git diff >> log.log
+git whatchanged >> log.log
+git "!sh -c 'if [ $# -eq 0 ] ; then git diff HEAD~1 HEAD ; else git diff HEAD~`expr $1 + 1` HEAD~$1 ; fi' -" >> log.log
