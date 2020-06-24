0cf414272fafc28fc3f5a370b0288972823831f3
Baishali Laskar <Baishali.Laskar@gds.ey.com>
2020-06-25 02:14:34 +0530

    Update script

--- generateLog.sh
+++ generateLog.sh
@@ -1,3 +1,3 @@
 ls -ltr > log.log
 git whatchanged >> log.log
-git "!sh -c 'if [ $# -eq 0 ] ; then git diff HEAD~1 HEAD ; else git diff HEAD~`expr $1 + 1` HEAD~$1 ; fi' -" >> log.log
+git diff HEAD~1 HEAD >> log.log
