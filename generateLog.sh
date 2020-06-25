63e7092eb1361fe71b2f091c162f13a4b02cc13b
blaskar <30313971+blaskar@users.noreply.github.com>
2020-06-26 01:52:15 +0530

    Update generateLog.sh

--- generateLog.sh
+++ generateLog.sh
@@ -1,5 +1,9 @@
 #git log -1 > temp.txt
 #var=`cat temp.txt | awk '/commit/ {print $2}'`
 #git diff -- "$var" > log.log
-#git log -1 --name-only | grep -i commit | awk {print $2} > log.log
-git log -1
+#git log -1 --name-only | grep -i commit | awk {print $2} > log.l> temp.txt
+git log -1 > temp
+var=`head -1 temp | awk '{print $2}'`
+echo $var
+#git show --pretty="format:" --name-only "$var"
+
