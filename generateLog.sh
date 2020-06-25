ac5f6401fb21b1ac20cb6aaaa505668f2ddb8cff
blaskar <30313971+blaskar@users.noreply.github.com>
2020-06-26 03:15:55 +0530

    Update generateLog.sh

--- generateLog.sh
+++ generateLog.sh
@@ -2,9 +2,9 @@
 #var=`cat temp.txt | awk '/commit/ {print $2}'`
 #git diff -- "$var" > log.log
 #git log -1 --name-only | grep -i commit | awk {print $2} > log.l> temp.txt
-git log -1 > temp1
+#git log -1 > temp1
 #git rev-parse @~ > temp2
-var1=`head -1 temp1 | awk '{print $2}'`
+#var1=`head -1 temp1 | awk '{print $2}'`
 #var2=`head -1 temp2 | awk '{print $2}'`
 #echo $var1
 #echo $var2
@@ -12,9 +12,9 @@ var1=`head -1 temp1 | awk '{print $2}'`
 #git diff "$var" -- "$var2"
 #git whatchanged
 #git log
-env
+#env
 url="https://api.github.com/repos/blaskar/cicd-implementation/commits/'${GITHUB_SHA}'"
 echo $url
-respone=`curl -o out.json https://api.github.com/repos/blaskar/cicd-implementation/commits/"${GITHUB_SHA}"`
-file=`echo $response | grep files` 
-echo $file
+#respone=`curl -o out.json https://api.github.com/repos/blaskar/cicd-implementation/commits/"${GITHUB_SHA}"`
+#file=`echo $response | grep files` 
+#echo $file
