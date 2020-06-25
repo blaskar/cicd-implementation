bcd69e8f08d5f8a66367a0592e6d655993096d69
blaskar <30313971+blaskar@users.noreply.github.com>
2020-06-26 01:40:35 +0530

    Update generateLog.sh

--- generateLog.sh
+++ generateLog.sh
@@ -1,4 +1,5 @@
-git log -p -1 > temp.txt
-var=`tail -f temp.txt | awk '/commit/ {print $2}'`
-git diff -- "$var" > log.log
+#git log -1 > temp.txt
+#var=`cat temp.txt | awk '/commit/ {print $2}'`
+#git diff -- "$var" > log.log
 #git log -1 --name-only | grep -i commit | awk {print $2} > log.log
+git log -1
