aa291843dbc4b265e8f3023aff86f77227edd1d5
blaskar <30313971+blaskar@users.noreply.github.com>
2020-06-26 03:05:20 +0530

    Update generateLog.sh

--- generateLog.sh
+++ generateLog.sh
@@ -12,6 +12,7 @@ var1=`head -1 temp1 | awk '{print $2}'`
 #git diff "$var" -- "$var2"
 #git whatchanged
 #git log
+env
 url = "curl -o out.json https://api.github.com/repos/blaskar/cicd-implementation/git/commits/${GITHUB_SHA}"
 echo $url
 respone=`curl -o out.json https://api.github.com/repos/blaskar/cicd-implementation/git/commits/${GITHUB_SHA}`
