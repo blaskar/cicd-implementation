81dae926c2aa92cacf8423c8f6f915e907d2bf5d
blaskar <30313971+blaskar@users.noreply.github.com>
2020-06-26 21:40:14 +0530

    Update generateLog.sh

--- generateLog.sh
+++ generateLog.sh
@@ -17,6 +17,6 @@ echo "Latest Commit: $GITHUB_SHA " > commitLog.log
 printf "%s\nFiles changed:%s\n" >> commitLog.log
 curl --request GET https://api.github.com/repos/blaskar/cicd-implementation/commits/${GITHUB_SHA} \ > response.txt
 #cat response.txt | grep -w 'filename\|status' >> commitLog.log
-cat response.txt | grep -w 'filename\|status' | awk '{printf  $2, "%s\n" , $4}' | cut -d '"' -f 2 | tr "@@" '\n' >> commitLog.log
+cat response.txt | grep -w 'filename\|status' | awk '{printf  $2, $4}' | cut -d '"' -f 2 | tr "@@" '\n' >> commitLog.log
 #cat response.txt | grep status | awk '{printf "%s\n", $2, "%s\n"}' | cut -d '"' -f 2 | tr "@@" '\n' >> log.log
 
